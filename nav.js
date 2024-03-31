f5e72014b071ffc057269613342aae97531a161f
Anmol Govinda Rao <53160535+agraaaaao@users.noreply.github.com>
2024-03-31 09:33:19 -0700

    Delete all code

--- nav.js
+++
@@ -1,9 +0,0 @@
-// Nav
-function toggleNav() {
-	const nav = document.querySelector('.nav');
-	if (parseInt(nav.dataset.active) == 1) {
-		nav.dataset.active = 0;
-	} else {
-		nav.dataset.active = 1;
-	}
-}
